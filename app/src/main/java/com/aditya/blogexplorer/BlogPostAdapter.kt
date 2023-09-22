6f3db4ab59df0a0eccbc36d1fd6d60097b7321dc
Aditya Mishra <adityamishra2217@gmail.com>
2023-09-22 12:44:17 +0530

    Corrected the order in Adapter and also implemented dialogue for back press

--- app/src/main/java/com/aditya/blogexplorer/BlogPostAdapter.kt
+++ app/src/main/java/com/aditya/blogexplorer/BlogPostAdapter.kt
@@ -1,11 +1,8 @@
 package com.aditya.blogexplorer
 
-import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.aditya.blogexplorer.databinding.ItemBlogPostBinding
 import com.aditya.blogexplorer.models.Post
@@ -13,31 +10,32 @@ import com.aditya.blogexplorer.models.Post
 private const val TAG = "BlogAdapter"
 
 class BlogPostAdapter(private val posts: List<Post>, private val itemClicked: ItemClickedListener) :
-    RecyclerView.Adapter<BlogPostAdapter.ViewHolder>() {
+    RecyclerView.Adapter<BlogPostAdapter.BlogViewHolder>() {
     lateinit var itemView: ItemBlogPostBinding
 
     interface ItemClickedListener {
         fun onItemClicked(post: Post)
     }
 
-    inner class ViewHolder(itemView: ItemBlogPostBinding) :
-        RecyclerView.ViewHolder(itemView.root) {}
+    inner class BlogViewHolder(val holderView: ItemBlogPostBinding) :
+        RecyclerView.ViewHolder(holderView.root) {}
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlogViewHolder {
         itemView = ItemBlogPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(itemView)
+        return BlogViewHolder(itemView)
     }
 
 
     override fun getItemCount() = posts.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BlogViewHolder, position: Int) {
         val post = posts[position]
-        itemView.tvId.text = post.id.toString()
-        itemView.tvTitle.text = post.title
-        itemView.tvBlogBody.text = post.title
+
+        holder.holderView.tvId.text = post.id.toString()
+        holder.holderView.tvTitle.text = post.title
+        holder.holderView.tvBlogBody.text = post.title
         Log.i(TAG, "onBindViewHolder: ")
-        itemView.root.setOnClickListener {
+        holder.holderView.root.setOnClickListener {
             Log.i(TAG, "On clicked Listened ")
             itemClicked.onItemClicked(post)
         }
